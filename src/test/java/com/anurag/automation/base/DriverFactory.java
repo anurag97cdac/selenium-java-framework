44b9cfa687b2c1a5db5e9308e2d3492ea92be160
anurag97cdac <anurag018@outlook.com>
2026-03-02 19:34:25 +0530

    browser change

--- src/test/java/com/anurag/automation/base/DriverFactory.java
+++ src/test/java/com/anurag/automation/base/DriverFactory.java
@@ -2,6 +2,7 @@ package com.anurag.automation.base;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
@@ -14,7 +15,13 @@ public class DriverFactory {
 
         switch (browser.toLowerCase()) {
             case "chrome":
-                driver = new ChromeDriver();
+                ChromeOptions options = new ChromeOptions();
+
+                if(System.getProperty("headless") != null && 
+                System.getProperty("headless").equals("true")){
+                    options.addArguments("--headless=new");
+                }
+                driver = new ChromeDriver(options);
                 break;
 
             case "edge":
